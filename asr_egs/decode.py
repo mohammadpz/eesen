addc4a1fe3b131e0918cade3579fdb63151bf3b8
Mohammad Pezeshki <mohammadpz@gmail.com>
2016-03-29 11:32:38 -0400

    stable for wsj char level

--- asr_egs/decode.py
+++ asr_egs/decode.py
@@ -1,8 +1,106 @@
 import numpy
-from picklable_itertools import groupby
 import kaldi_io
-import subprocess
+from collections import defaultdict
+from subprocess import call
 floatX = 'float32'
+
+
+ind_to_char = {
+    0: '-',
+    1: 'A',
+    2: 'C',
+    3: 'G',
+    4: 'K',
+    5: 'O',
+    6: 'S',
+    7: 'V',
+    8: ' ',  # <SPACE>
+    9: 'T',
+    10: 'B',
+    11: 'F',
+    12: 'J',
+    13: 'N',
+    14: 'R',
+    15: 'Y',
+    16: '<NOISE>',
+    17: "'",
+    18: 'E',
+    19: 'I',
+    20: 'M',
+    21: 'Q',
+    22: 'U',
+    23: 'X',
+    24: '.',
+    25: 'D',
+    26: 'H',
+    27: 'L',
+    28: 'P',
+    29: 'W',
+    30: 'Z'}
+
+softmax_to_fst = {
+    0: 0,
+    1: 14,
+    2: 30,
+    3: 32,
+    4: 36,
+    5: 40,
+    6: 44,
+    7: 48,
+    8: 51,
+    9: 3,
+    10: 49,
+    11: 31,
+    12: 35,
+    13: 39,
+    14: 43,
+    15: 47,
+    16: 54,
+    17: 1,
+    18: 10,
+    19: 34,
+    20: 38,
+    21: 42,
+    22: 46,
+    23: 50,
+    24: 53,
+    25: 15,
+    26: 33,
+    27: 37,
+    28: 41,
+    29: 45,
+    30: 52,
+    31: 55}
+
+
+def make_wsj_probs(net_output, log_probs_ark_path):
+    # a list of test examples, each with a shape of Time x 32
+    softmax_log_probs, trans = numpy.load(net_output)
+
+    fst_format_log_probs = [-10000 * numpy.ones((i.shape[0], 64))
+                            for i in softmax_log_probs]
+
+    for i in range(len(softmax_log_probs)):
+        for col in range(32):
+            new_col = softmax_to_fst[col]
+            fst_format_log_probs[i][:, new_col] = \
+                softmax_log_probs[i][:, col]
+
+    log_probs_ark_path = 'ark:' + log_probs_ark_path
+    writer = kaldi_io.BaseFloatMatrixWriter(log_probs_ark_path)
+    for i, exp in enumerate(fst_format_log_probs):
+        writer.write(str(i), exp)
+
+    all_mapped_trans = []
+    for i in range(len(trans)):
+        new_trans = []
+        for inx in trans[i]:
+            new_trans.append(softmax_to_fst[inx + 1])
+        all_mapped_trans.append(new_trans)
+
+    print "Ark file created"
+
+    return log_probs_ark_path, all_mapped_trans, trans
 
 
 def ctc_strip(sequence, blank_symbol=0):
@@ -204,58 +302,78 @@ def remove_duplicates(input_):
     return output
 
 
-def compute_wer_argmax(trans_path, probs_path):
-    trans = open(trans_path, 'r').readlines()
-    all_trans = []
-    for line in trans:
-        trans = [int(el) for el in line.strip('\n').split()[1:]]
-        trans = [ind_to_48_phones[ind] for ind in trans]
-        trans = [dict_48_to_39_phones[phone] for phone in trans]
-        all_trans.append(remove_duplicates(trans))
-
+def compute_wer_argmax(all_trans, probs_path):
     all_argmax_predict = []
     reader = kaldi_io.SequentialBaseFloatMatrixReader(probs_path)
     for name, value in reader:
         argmax_predict = ctc_strip(numpy.argmax(value, axis=1))
-        argmax_predict = [ind_to_48_phones[ind] for ind in argmax_predict]
-        argmax_predict = [dict_48_to_39_phones[phone] for
-                          phone in argmax_predict]
-        all_argmax_predict.append(remove_duplicates(argmax_predict))
+        argmax_predict = [i for i in argmax_predict]
+        all_argmax_predict.append(argmax_predict)
 
     return (numpy.sum(Evaluation.wer(all_argmax_predict, all_trans)) /
             float(len(all_argmax_predict)))
 
 
-def compute_wer(trans_path, predict_path):
-    trans = open(trans_path, 'r').readlines()
-    all_trans = []
-    for line in trans:
-        trans = [int(el) for el in line.strip('\n').split()[1:]]
-        trans = [ind_to_48_phones[ind] for ind in trans]
-        trans = [dict_48_to_39_phones[phone] for phone in trans]
-        all_trans.append(remove_duplicates(trans))
-
+def compute_wer(all_trans, predict_path):
     predict = open(predict_path, 'r').readlines()
     all_predict = []
     for line in predict:
-        line = line.strip('\n').split()[1:]
+        line = line.strip('\n').split()
+        line = line[1:]
         predict = ctc_strip([int(el) for el in line])
-        predict = [el - 1 for el in predict]
-        predict = [ind_to_48_phones[ind] for ind in predict]
-        predict = [dict_48_to_39_phones[phone] for phone in predict]
-        all_predict.append(remove_duplicates(predict))
+        predict = [el for el in predict]
+        all_predict.append(predict)
 
     return (numpy.sum(Evaluation.wer(all_predict, all_trans)) /
             float(len(all_predict)))
 
-print "Best Path Decoding WER: " + str(compute_wer(
-    trans_path='/u/pezeshki/eesen/asr_egs/files/trans.txt',
-    predict_path='/u/pezeshki/eesen/asr_egs/files/res_bestpath.ark'))
+net_output = '/u/pezeshki/speech_project/net_output.npy'
+log_probs_ark_path = '/u/pezeshki/eesen/asr_egs/files/probs.ark'
+log_probs_ark_path, all_new_trans, all_old_trans = make_wsj_probs(
+    net_output, log_probs_ark_path)
 
-print "LatGen Decoding WER: " + str(compute_wer(
-    trans_path='/u/pezeshki/eesen/asr_egs/files/trans.txt',
-    predict_path='/u/pezeshki/eesen/asr_egs/files/res_latgen.ark'))
+words_path = '/u/zhangy/lisa/eesen/asr_egs/wsj/data/lang_char_larger/words.txt'
+words_file = open(words_path, 'r').readlines()
+word_to_ind = {}
+for line in words_file:
+    line = line.strip('\n').split()
+    word_to_ind[line[0]] = int(line[1])
 
-print "Argmax Decoding WER: " + str(compute_wer_argmax(
-    trans_path='/u/pezeshki/eesen/asr_egs/files/trans.txt',
-    probs_path='ark:/u/pezeshki/eesen/asr_egs/files/probs.ark'))
+trans_word_level = []
+for example in all_old_trans:
+    string = ''
+    for ind in example:
+        string = string + ind_to_char[ind]
+    string = string.strip('\n').split()
+    trans = []
+    for word in string:
+        trans.append(word_to_ind[word])
+    trans_word_level.append(trans)
+
+
+fst_path = '/u/zhangy/lisa/eesen/asr_egs/wsj/data/lang_char_larger_test_tgpr/TLG.fst'
+tokens_path = '/u/zhangy/lisa/eesen/asr_egs/wsj/data/lang_char/tokens.txt'
+save_path = '/u/pezeshki/eesen/asr_egs/files/res.ark'
+
+acwts = [0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 2, 5, 8, 15, 30, 50, 100, 500]
+beams = [1, 5, 10, 15, 17, 20, 30, 70, 150, 300, 700, 1000]
+all_exps = []
+i = 0
+for acwt in acwts:
+    for beam in beams:
+        print 'EXPERIMENT:' + str(i)
+        i = i + 1
+        this_exp = {}
+        call(['sh', '/u/pezeshki/eesen/asr_egs/script_moh.sh',
+              str(beam), str(acwt), str(fst_path),
+              str(log_probs_ark_path), str(save_path),
+              str(tokens_path)])
+
+        print "Beam search:"
+        print str(compute_wer(trans_word_level, save_path))
+        this_exp['acwt'] = acwt
+        this_exp['beam'] = beam
+        this_exp['wer'] = compute_wer(trans_word_level, save_path)
+        print this_exp
+        all_exps.append(this_exp)
+numpy.save('all_exps', all_exps)
--- asr_egs/script_moh.sh
+++ asr_egs/script_moh.sh
@@ -5,38 +5,14 @@ echo =====================================================================
 nj=1
 max_active=7000
 min_active=20
-beam=1000.0
+beam=$1
 beam_delta=1.0
-acwt=5.0
+acwt=$2
 
-# run.pl JOB=1:$nj files/decode.JOB.log \
 decode-faster --acoustic-scale=$acwt --allow-partial=true \
     --beam=$beam --beam-delta=$beam_delta --binary=true \
     --max-active=$max_active --min-active=$min_active \
-    files/TG.fst \
-    ark:files/probs.ark \
-    ark,t:files/res_bestpath.ark
-
-echo =====================================================================
-echo "                         Lat Gen Decoding                          "
-echo =====================================================================
-
-nj=1
-max_active=7000
-max_mem=50000000
-beam=1500.0
-lattice_beam=10
-acwt=1.0 # acwt: acoustic weight used in getting lattices
-acwt_factor=1 
-
-latgen-faster  --max-active=$max_active --max-mem=$max_mem --beam=$beam \
-    --lattice-beam=$lattice_beam --acoustic-scale=$acwt \
-    --allow-partial=true \
-    files/TG.fst \
-    ark:files/probs.ark \
-    ark:files/lat
-
-lattice-scale --acoustic-scale=$acwt --ascale-factor=$acwt_factor \
-    ark:files/lat ark:files/lat_scale.ark 
-
-lattice-best-path ark:files/lat_scale.ark  ark,t:files/res_latgen.ark
+    --word-symbol-table=/u/zhangy/lisa/eesen/asr_egs/wsj/data/lang_char_larger/words.txt \
+    $3 \
+    $4 \
+    ark,t:$5
